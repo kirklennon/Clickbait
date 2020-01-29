7b9c1ae33c8fc7a13a8e53768a7f3e2bb7fc1c10
Kirk Lennon <kirklennon@me.com>
2020-01-28 21:26:01 -0800

    Update search.py

--- search.py
+++ search.py
@@ -5,12 +5,11 @@ conn = sqlite3.connect('db.sqlite3')
 cur = conn.cursor()
 
 subscription_key = 'CONFIDENTIAL'
-search_term = '"apple" ("shiny" OR "dump" OR "analyst" OR "rumor")'
+search_term = '"apple" ("shiny" OR "analyst" OR "rumor" OR "nasty" OR "cult")'
 search_url = 'https://clickbait-searcher.cognitiveservices.azure.com/bing/v7.0/news/search'
 
 headers = {'Ocp-Apim-Subscription-Key' : subscription_key}
-params  = {'q': search_term, }
-# 'freshness': 'Week'
+params  = {'q': search_term, 'freshness': 'Week', 'count': 100}
 
 response = requests.get(search_url, headers=headers, params=params)
 response.raise_for_status()
@@ -21,9 +20,9 @@ for article in search_results['value']:
 	if '?' in url:
 		url = url.split('?', 1)[0]
 	try:
-		cur.execute('INSERT OR IGNORE INTO Source (url, visited) VALUES (?, 0)', (url,))
+		cur.execute('INSERT OR IGNORE INTO api_source (url, visited) VALUES (?,  0)', (url,))
 	except:
 		print('failed to save: ', url)
-
+print('Search complete.')
 conn.commit()
 conn.close()
